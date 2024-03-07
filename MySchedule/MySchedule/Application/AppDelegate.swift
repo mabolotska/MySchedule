0ef70934e754df883a62bbe28b9cf081c8ba00d6
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2024-03-07 10:33:44 +0100

    need to fix nav bar color

---
+++ MySchedule/MySchedule/Application/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MySchedule
+//
+//  Created by Maryna Bolotska on 06/03/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainTabBarController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+}
+
---
+++ MySchedule/MySchedule/Controllers/ContactsViewController.swift
@@ -0,0 +1,13 @@
+
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Contacts"
+    }
+    
+}
